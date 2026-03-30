698577a1d889225511ca0b73dafa2b2872e1424a
I-jidd <jade.notsopersonal@gmail.com>
2026-03-30 11:15:17 +0800

    feat: add JWT utility class

---
+++ backend/src/main/java/com/jobtracker/backend/security/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.jobtracker.backend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("$jwt.expiration")
+    private Long expiration;
+
+    public String generateToken(String email){
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        try{
+            return !extractClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractClaims(String token){
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
